7d1034af78d4e82dc3c0d782a110e71d441eab3e
danilobrayann <danilobrayann@gmail.com>
2024-03-14 10:47:01 -0300

    fix: correção

--- src/container/Home/Home.tsx
+++ src/container/Home/Home.tsx
@@ -1,31 +1,26 @@
-import { useState } from "react"
-import Header from "../../components/Header"
-import Carrossel from '../../components/Carrossel'
-import ContatoFlutuante from '../../components/ContatoFlutuante/index'
-import Parceria from "../../components/SectionParceria"
-import Servicos from "../../components/servicos"
-import ButtonTop from '../../components/buttontop'
-import MenuMobile from '../../components/MenuMobile'
-export default function Home () {
-const [menuInvisibal, setmenuInvisibal] = useState(false);
+import { useState } from "react";
+import Header from "../../components/Header";
+import Carrossel from "../../components/Carrossel";
+import ContatoFlutuante from "../../components/ContatoFlutuante/index";
+import Parceria from "../../components/SectionParceria";
+import Servicos from "../../components/servicos";
+import ButtonTop from "../../components/buttontop";
+import MenuMobile from "../../components/MenuMobile";
+export default function Home() {
+  const [menuInvisibal, setmenuInvisibal] = useState(false);
 
-
-return (
-
+  return (
     <>
-    <MenuMobile
-    menuInvisibal={menuInvisibal}
-    setmenuInvisibal={setmenuInvisibal}
-    />
-   <Header setmenuInvisibal={setmenuInvisibal}/>
-   <Carrossel/>
-   <ContatoFlutuante/>
-   <Parceria/>
-   <Servicos/>
-   <ButtonTop/>
+      <MenuMobile
+        menuInvisibal={menuInvisibal}
+        setmenuInvisibal={setmenuInvisibal}
+      />
+      <Header setmenuInvisibal={setmenuInvisibal} />
+      <Carrossel />
+      <ContatoFlutuante />
+      <Parceria />
+      <Servicos />
+      <ButtonTop />
     </>
-)
-
-
-    
+  );
 }
